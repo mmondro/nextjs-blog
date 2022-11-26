978c3ad4a26d52cf8de3395a30749db1b8bb701f
Mitch <mmondro@clarku.edu>
2022-11-25 21:58:23 -0500

    NEW NEW

--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,7 @@ export default function Home() {
               Marketing Expert
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-            This is a test.
+            Hello World
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               
